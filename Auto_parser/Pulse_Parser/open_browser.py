54aa64b394c9551aee4fdd5bed386e27795863df
PazhiloyMaimalyonok <mrparav0z13@gmail.com>
2020-07-23 09:55:33 +0300

    Update open_browser.py

--- Auto_parser/Pulse_Parser/open_browser.py
+++ Auto_parser/Pulse_Parser/open_browser.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
-driver.get('https://www.google.com/')
-print(driver.current_url)
-driver.quit()
+try:
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
+    driver.get('https://www.google.com/')
+    print(driver.current_url)
+    driver.quit()
+    print('Success')
+except:
+    print('zopa')
