9da7b1c2cb99541c8267285a6af205f9d3231dff
PazhiloyMaimalyonok <mrparav0z13@gmail.com>
2020-07-23 09:54:27 +0300

    Update open_browser.py

--- Auto_parser/Pulse_Parser/open_browser.py
+++ Auto_parser/Pulse_Parser/open_browser.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
 driver.get('https://www.google.com/')
 print(driver.current_url)
 driver.quit()
